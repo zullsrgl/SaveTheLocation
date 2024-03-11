81d4ae253d9e203d2918e91212a47bd9e574a2a5
zulalsarioglu <zulal.sariogluu@gmail.com>
2024-03-12 00:14:47 +0300

    Initial Commit

---
+++ SaveTheLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaveTheLocation
+//
+//  Created by Zülal Sarıoğlu on 12.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
